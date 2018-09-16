e7ceba6875832a9c8b30a048621d75566701696b
Duong The Cuong <duongthecuongbk227@gmail.com>
2018-09-17 00:53:29 +0700

    update_bt4_trang172

--- src/quy_hoach_dong/bai_tap/trang_170_co_huong_dan/BaiTap4.java
+++ src/quy_hoach_dong/bai_tap/trang_170_co_huong_dan/BaiTap4.java
@@ -19,24 +19,6 @@ public class BaiTap4 {
         return max;
     }
 
-    private int maxI(int x, int y, int z) {
-        int max;
-        if (x < y) {
-            max = y;
-        } else {
-            max = x;
-        }
-        if (z > max) {
-            max = z;
-        }
-        if (max == x) {
-            return 1;
-        } else if (max == y) {
-            return 2;
-        }
-        return 3;
-    }
-
     private int maxArr(int[] A) {
         int max = A[0];
         int x = 0;
@@ -75,20 +57,14 @@ public class BaiTap4 {
         System.out.println("------------------------");
 
         while (n - 1 != 0) {
-            if (maxI(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == 1) {
-                System.out.print("A[" + k + "][" + (n - 1) + "]" + " \t");
-            } else if (maxI(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == 2) {
-                System.out.print("A[" + (k - 1) + "][" + (n - 1) + "]" + " \t");
-            } else if (maxI(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == 3) {
-                System.out.print("A[" + (k + 1) + "][" + (n - 1) + "]" + " \t");
-            }
-            if (maxI(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == 1) {
+            if (max(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == B[k][n - 1]) {
                 k += 0;
-            } else if (maxI(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == 2) {
-                k = k - 1;
-            } else if (maxI(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == 3) {
-                k = k + 1;
+            } else if (max(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == B[k - 1][n - 1]) {
+                k -= 1;
+            } else if (max(B[k][n - 1], B[k - 1][n - 1], B[k + 1][n - 1]) == B[k + 1][n - 1]) {
+                k += 1;
             }
+            System.out.print("A[" + k + "][" + (n - 1) + "]" + " \t");
             n--;
         }
 

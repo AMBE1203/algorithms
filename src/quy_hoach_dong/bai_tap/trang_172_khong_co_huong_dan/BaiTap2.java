c027c5769e09fe4c8fcb1da1ada1c101062ac3eb
Duong The Cuong <duongthecuongbk227@gmail.com>
2018-09-25 03:11:07 +0700

    u

---
+++ src/quy_hoach_dong/bai_tap/trang_172_khong_co_huong_dan/BaiTap2.java
@@ -0,0 +1,10 @@
+package quy_hoach_dong.bai_tap.trang_172_khong_co_huong_dan;
+
+/**
+ * Created by AMBE on 09/17/2018 at 01:02.
+ * Xâu ký tự S gọi là xâu con của xâu ký tự T nếu có thể xóa bớt 1 số ký tự trong xâu T để
+ * được xâu S. Lập chương trình nhập vào 2 xâu ký tự X,Y. Tìm xâu Z có độ dài lơn nhất là
+ * xâu con của cả X và Y.
+ */
+public class BaiTap2 {
+}

18e39dfdcebda8bd43a47ef781e9ddacf879481b
Your Name <you@example.com>
2018-11-13 11:55:01 +0700

    u

---
+++ src/quy_hoach_dong/bai_tap/trang_172_khong_co_huong_dan/BaiTap3.java
@@ -0,0 +1,14 @@
+package quy_hoach_dong.bai_tap.trang_172_khong_co_huong_dan;
+
+/**
+ * Created by AMBE on 11/13/2018 at 11:39 AM.
+ * Một xâu ký tự X gọi là chứa xâu ký tự Y nếu như có thể xóa bớt 1 số ký tự trong xâu X
+ * để được xâu Y: Ví dụ xâu 1a2b3c45d chứa xâu 12345. Một xâu ký tự gọi là đối xứng nếu nó không
+ * thay đổi khi ta viết các ký tự trong xâu theo thứ tự ngược lại.
+ * Nhập 1 xâu ký tự S có độ dài không quá 128, hãy tìm xâu ký tự T thỏa mãn 3 điều kiện
+ * Đối xứng
+ * Chứa xâu S
+ * Có ít ký tự nhất ( độ dài ngắn nhất )
+ */
+public class BaiTap3 {
+}
